e292c8b44ab53c29fe47210548053917a15f6446
Sowtch <quentin.cptr@gmail.com>
2021-02-25 17:09:24 +0100

    update Q2

--- CPL.hs
+++ CPL.hs
@@ -9,29 +9,35 @@
 module CPL where
 
 {- Type structuré Formula pour représenter des formules booléennes (c.-à-d., des formules logiques).-}
-data Formula a = T | F | Var [Char]
-    | Not   (Formula a)
-    | And   (Formula a) (Formula a)
-    | Or    (Formula a) (Formula a)
-    | Imp   (Formula a) (Formula a)
-    | Eqv   (Formula a) (Formula a)
+data Formula = T | F | Var [Char]
+    | Not   (Formula)
+    | And   (Formula) (Formula)
+    | Or    (Formula) (Formula)
+    | Imp   (Formula) (Formula)
+    | Eqv   (Formula) (Formula)
     deriving (Eq)
 
-instance Show (Formula a) where
+instance Show Formula where
     show T = "Vrai"
     show F = "Faux"
-    show (Var v) = v
+    show (Var s) = s
     show (Not f) = "~" ++ show f
     show (And f1 f2) = "(" ++ show f1 ++ " ∧ " ++ show f2 ++ ")"
     show (Or f1 f2) = "(" ++ show f1 ++ " ∨ " ++ show f2 ++ ")"
     show (Imp f1 f2) = "(" ++ show f1 ++ " ⇒ " ++ show f2 ++ ")"
     show (Eqv f1 f2) = "(" ++ show f1 ++ " ⇔ " ++ show f2 ++ ")"
 
-
-constraint :: Formula a
+{- Formule qui décrit le fait qu’il ne peut pas y avoir un tigre et une princesse (en même temps) dans chaque cellule. -}
+constraint :: Formula
 constraint = Or (And (Not (Var "p1")) (Var "t1")) (Or (Var "p2") (Not (Var "t2")))
 
+{- Formule pour exprimer le fait que, dans la première épreuve au moins, l'une des portes dit la vérité et l'autre ment. -}
+reglement :: Formula
+reglement = Or (And (Not (Var "1")) (Var "2")) (And (Var "1") (Not (Var "2")))
 
+{- Fonction qui fait la conjonction de toutes les formules de la premi`ere ´epreuve. -}
+challenge1 :: Formula
+challenge1 = And (constraint) (reglement)
 
 
 {- Type World, pour représenter le type des mondes possibles. -}
