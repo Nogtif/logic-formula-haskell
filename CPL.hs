626d80b65e9195ab4a98146c6bfa4976be25886c
Sowtch <quentin.cptr@gmail.com>
2021-03-01 17:24:16 +0100

    update

--- CPL.hs
+++ CPL.hs
@@ -27,21 +27,32 @@ instance Show Formula where
     show (Imp f1 f2) = "(" ++ show f1 ++ " ⇒ " ++ show f2 ++ ")"
     show (Eqv f1 f2) = "(" ++ show f1 ++ " ⇔ " ++ show f2 ++ ")"
 
+door1 :: Formula
+door1 = And (Var "p1") (Var "t2")
+
+door2 :: Formula
+door2 = Or (And (Var "p1") (Var "t2")) (And (Var "t1") (Var "p2"))
+
 {- Formule qui décrit le fait qu’il ne peut pas y avoir un tigre et une princesse (en même temps) dans chaque cellule. -}
 constraint :: Formula
-constraint = Or (And (Not (Var "p1")) (Var "t1")) (Or (Var "p2") (Not (Var "t2")))
+constraint = And (Eqv (Var "p1") (Not (Var "t1"))) (Eqv (Var "p2") (Not (Var "t2")))
 
 {- Formule pour exprimer le fait que, dans la première épreuve au moins, l'une des portes dit la vérité et l'autre ment. -}
 reglement :: Formula
-reglement = Or (And (Not (Var "1")) (Var "2")) (And (Var "1") (Not (Var "2")))
+reglement = Or (And (Not door1) door2) (And door1 (Not door2))
 
-{- Fonction qui fait la conjonction de toutes les formules de la premi`ere ´epreuve. -}
+{- Fonction qui fait la conjonction de toutes les formules de la première épreuve. -}
 challenge1 :: Formula
 challenge1 = And (constraint) (reglement)
 
-
 {- Type World, pour représenter le type des mondes possibles. -}
 type World = [[Char]]
+
+isExist :: World -> [World] -> Bool
+isExist n [] = False
+isExist n (x:xs)
+    | n == x = True
+    | otherwise = isExist n xs
 
 {- Fonction qui, pour une liste de noms de variables propositionnels (tel que ["p1", "p2", "t1", "t2"]), 
 génère la liste de tous les mondes possibles pour ces variables. -}
