2f91b8d95cdfdd4ec8379ef94861bad8d399abc5
Sowtch <quentin.cptr@gmail.com>
2021-03-03 12:11:22 +0100

    ajout de la fonction genAllWorlds

--- CPL.hs
+++ CPL.hs
@@ -1,5 +1,4 @@
 {-# OPTIONS_GHC -Wall #-}
-
 {----- 
     LCPF : Projet
     Une princesse ou un tigre ?
@@ -8,7 +7,7 @@
 
 module CPL where
 
-{- Type structuré Formula pour représenter des formules booléennes (c.-à-d., des formules logiques).-}
+-- Type structuré Formula pour représenter des formules booléennes (c.-à-d., des formules logiques).
 data Formula = T | F | Var [Char]
     | Not   (Formula)
     | And   (Formula) (Formula)
@@ -33,11 +32,11 @@ door1 = And (Var "p1") (Var "t2")
 door2 :: Formula
 door2 = Or (And (Var "p1") (Var "t2")) (And (Var "t1") (Var "p2"))
 
-{- Formule qui décrit le fait qu’il ne peut pas y avoir un tigre et une princesse (en même temps) dans chaque cellule. -}
+-- Formule qui décrit le fait qu’il ne peut pas y avoir un tigre et une princesse (en même temps) dans chaque cellule.
 constraint :: Formula
 constraint = And (Eqv (Var "p1") (Not (Var "t1"))) (Eqv (Var "p2") (Not (Var "t2")))
 
-{- Formule pour exprimer le fait que, dans la première épreuve au moins, l'une des portes dit la vérité et l'autre ment. -}
+-- Formule pour exprimer le fait que, dans la première épreuve au moins, l'une des portes dit la vérité et l'autre ment. --
 reglement :: Formula
 reglement = Or (And (Not door1) door2) (And door1 (Not door2))
 
@@ -45,16 +44,18 @@ reglement = Or (And (Not door1) door2) (And door1 (Not door2))
 challenge1 :: Formula
 challenge1 = And (constraint) (reglement)
 
+
+
+
 {- Type World, pour représenter le type des mondes possibles. -}
 type World = [[Char]]
-
-isExist :: World -> [World] -> Bool
-isExist n [] = False
-isExist n (x:xs)
-    | n == x = True
-    | otherwise = isExist n xs
 
 {- Fonction qui, pour une liste de noms de variables propositionnels (tel que ["p1", "p2", "t1", "t2"]), 
 génère la liste de tous les mondes possibles pour ces variables. -}
 genAllWorlds :: [[Char]] -> [World]
-genAllWorlds _ = [["p1", "p2", "t1", "t2"]]
+genAllWorlds [] = []
+genAllWorlds (x:xs) = [x] : map (x :) (genAllWorlds xs) ++ genAllWorlds xs
+
+{- Fonction qui, pour un monde possible w et une formule phi pass´es en arguments, v´erifie si w satisfait phi. -}
+sat :: World -> Formula -> Bool
+sat w phi = True
