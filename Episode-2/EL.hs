eb20b11d5f0b10e3dc2206b672aa3138f54336e2
Sowtch <quentin.cptr@gmail.com>
2021-03-15 23:08:37 +0100

    chap2

--- Episode-2/EL.hs
+++ Episode-2/EL.hs
@@ -11,6 +11,8 @@ module EL (
     World,
     EpiState,
     EpiFormula (..),
+    epiSat, testEpiSat,
+    testAll
 ) where
 
 -- Représente une proposition.
@@ -35,4 +37,34 @@ data EpiFormula = T | F | Var [Char]
     | Eqv   (EpiFormula) (EpiFormula)
     | Knows [Char] (EpiFormula)
     | After (EpiFormula) (EpiFormula)
-    deriving (Eq)
+    deriving (Show, Eq)
+
+{- Fonction qui prend un état épistémiques et une formule phi en arguments,
+et renvoie True si a satisfait phi, et False sinon -}
+epiSat :: EpiState -> EpiFormula -> Bool
+epiSat _ T = True
+epiSat _ F = False
+epiSat (interp, _, w) (Var p) = w `elem` (interp p)
+epiSat s (Not phi) = not (epiSat s phi)
+epiSat s (And phi psi) = (epiSat s phi) && (epiSat s psi)
+epiSat s (Or phi psi)  = (epiSat s phi) || (epiSat s psi)
+epiSat s (Imp phi psi) = (not (epiSat s phi)) || (epiSat s psi)
+epiSat s (Eqv phi psi) = (epiSat s (Imp phi psi)) && (epiSat s (Imp psi phi))
+epiSat (interp, indis, w) (Knows a phi) = (epiSat (interp, indis, w) phi) && null (indis a w)
+
+testEpiSat :: [Bool]
+testEpiSat = [True]
+
+{- Fonction qui reçoit les résultats d’un test et qui retourne vrai si tous les résultats du test sont vrai et faux sinon. -}
+test :: [Bool] -> Bool
+test(tab)
+    | head tab == last tab && head tab == True = True
+    | head tab == False = False
+    | otherwise = test (tail tab)
+
+{- Fonction qui retourne la chaine de caractères "Success!" 
+si tous les résultats des tests de toutes les fonctions sont vrais, sinon "Fail!". -}
+testAll :: [Char]
+testAll 
+    | test(testEpiSat) = "Success"
+    | otherwise = "Fail!"
--- Episode-2/Problem1.hs
+++ Episode-2/Problem1.hs
@@ -8,11 +8,37 @@
 module Problem1 where
 import EL
 
+{- Fonction qui prend une proposition enargument 
+et renvoie la liste de mondes possibles où la proposition est vraie. -}
 interp :: Prop -> [World]
 interp "as" = [10 ,11]
 interp "bs" = [01 ,11]
 interp _    = []
 
-indis :: Agent  -> World  -> [World]
+{- Fonction qui prend un agent i et un monde possible w en arguments,
+et renvoie la liste de mondes possibles qui sont indiscernables du monde w pour l'agent i. -}
+indis :: Agent -> World -> [World]
 indis "a" 00 = [00 ,10]
 indis "b" 00 = [00 ,01]
+
+{- Définition complète de l'état épistémique initial du problème. -}
+s0 :: EpiState
+s0 = (interp, indis, 01)
+
+{- Exprime l'annonce du père, c.-à-d., « Un parmi vous a le visage sale. » -}
+fatherAnn :: EpiFormula
+fatherAnn = Or (Var "as") (Var "bs")
+
+{- Exprime l'ignorance d'Alice sur son état, « Alice ne sait pas que le visage d'Alice est sale, 
+et Alice ne sait pas que le visage d'Alice n’est pas sale. » -}
+aliceIgn :: EpiFormula
+aliceIgn = Not (And (Knows "a" (Var "as")) (Knows "a" (Not (Var "as"))))
+
+{- Exprime l'ignorance de Bob sur son état, « Bob ne sait pas que le visage de Bob est sale, 
+et Bob ne sait pas que le visage de Bob n’est pas sale. » -}
+bobIgn :: EpiFormula
+bobIgn = Not (And (Knows "b" (Var "bs")) (Knows "b" (Not (Var "bs"))))
+
+{- Exprime le problème 1 dans sa totalité. -}
+problem1 :: EpiFormula
+problem1 = After (fatherAnn) (And (aliceIgn) (After (Knows "b" (Not (Var "bs"))) (And (Not aliceIgn) (Not bobIgn))))
