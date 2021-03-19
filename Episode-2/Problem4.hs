c7b7a2aec9463b91bd90674bc00032c7456c98c8
Sowtch <quentin.cptr@gmail.com>
2021-03-20 00:32:51 +0100

    update

--- Episode-2/Problem4.hs
+++ Episode-2/Problem4.hs
@@ -11,55 +11,56 @@ import EL
 {- Fonction qui prend une proposition en argument,
 et renvoie la liste de mondes possibles où la proposition est vraie. -}
 interp :: Prop -> [World]
-interp "an" = [10, 12, 21, 23, 32, 34, 43]
-interp "bn" = [01, 21, 12, 32, 23, 43, 34]
+interp "an" = [01, 10, 12, 21, 23, 32, 34, 43]
+interp "bn" = [01, 10, 12, 21, 23, 32, 34, 43]
 interp _    = []
 
 {- Fonction qui prend un agent i et un monde possible w en arguments,
 et renvoie la liste de mondes possibles qui sont indiscernables du monde w pour l'agent i. -}
 indis :: Agent -> World -> [World]
+-- Monde indiscernable d'Anne :
+indis "a" 01 = []
 indis "a" 10 = [10, 12]
 indis "a" 12 = [12, 10]
 indis "a" 21 = [21, 23]
 indis "a" 23 = [23, 21]
 indis "a" 32 = [32, 34]
 indis "a" 34 = [34, 32]
-indis "a" 43 = [43]
-
+indis "a" 43 = []
+-- Monde indiscernable de Bill :
 indis "b" 01 = [01, 21]
-indis "b" 21 = [21, 01]
+indis "b" 10 = []
 indis "b" 12 = [12, 32]
-indis "b" 32 = [32, 12]
+indis "b" 21 = [21, 01]
 indis "b" 23 = [23, 43]
+indis "b" 32 = [32, 12]
+indis "b" 34 = []
 indis "b" 43 = [43, 23]
-indis "b" 34 = [34]
+-- Autres...
 indis _ _ = []
 
 {- Définition complète de l'état épistémique initial du problème. -}
 s0 :: EpiState
-s0 = (interp, indis, 12)
+s0 = (interp, indis, 32)
 
 {- Exprime l'ignorance d'Anne, « Anne ne connaît pas le nombre de Bill. » -}
 anneIgn :: EpiFormula
-anneIgn = And 
-            (Not (Knows "a" (Var "bn"))) 
-            (Not (Knows "a" (Not (Var "bn"))))
+anneIgn = (Not (Knows "a" (Var "bn"))) 
 
 {- Exprime l'ignorance de Bill, « Bill ne sait pas que le visage de Bill est sale, 
 et Bill ne sait pas que le visage de Bill n’est pas sale. » -}
 billIgn :: EpiFormula
-billIgn = And 
-        (Not (Knows "b" (Var "an"))) 
-        (Not (Knows "b" (Not (Var "an"))))
+billIgn = (Not (Knows "b" (Var "an"))) 
 
 {- Exprime le problème 4 dans sa totalité :
-« Anne ne connaît pas le nombre de Bill, 
-et Bill ne connaît pas le nombre de Anne, et Après que Anne annonce son ignorance,
-et après que Bill annonce son ignorance, Anne connaît le nombre de Bill. » -}
+« Anne ne connaît pas le nombre de Bill, et Bill ne connaît pas le nombre de Anne, 
+et Après que Anne annonce son ignorance, et après que Bill annonce son ignorance, 
+Anne connaît le nombre de Bill. » -}
 problem4 :: EpiFormula
 problem4 = And 
-            (And anneIgn billIgn)
-            (After 
-                anneIgn 
-                (After billIgn (Not anneIgn))
+            (And anneIgn billIgn) 
+            (After anneIgn (And 
+                                (And anneIgn billIgn)
+                                (After billIgn (Not anneIgn)) 
+                            ) 
             )
